cf236efb1e6c7adcc3d7200ae649ac9fe067cfc8
twa127 <46624596+twa127@users.noreply.github.com>
2025-10-15 17:19:37 +0100

    Update install.sh

--- add_on/zigbee2mqtt/install.sh
+++ add_on/zigbee2mqtt/install.sh
@@ -32,15 +32,9 @@ echo "Installing dependencies"
 cd /opt/zigbee2mqtt
 pnpm install --frozen-lockfile
 
-echo "Creating /opt/zigbee2mqtt/data/configuration.yaml file"
-sudo cp configuration.yaml /opt/zigbee2mqtt/data
-
 # Create service
 echo "Creating service for auto start"
 sudo cp /var/www/add_on/zigbee2mqtt/zigbee2mqtt.service /etc/systemd/system/zigbee2mqtt.service
 sudo systemctl enable zigbee2mqtt.service
 
-echo "Starting the service"
-sudo systemctl start zigbee2mqtt.service
-
-echo "Finished"
+echo -e "From the command line enter 'sudo systemctl start zigbee2mqtt.service'.\nThen brows to xxx.xxx.xxx.xxx8080 to complete the 'onboarding' stage of the install process."
