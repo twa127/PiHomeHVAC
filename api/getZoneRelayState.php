e577e167f735f22a4ef20e77c8f2ff5afb477653
twa127 <46624596+twa127@users.noreply.github.com>
2026-05-08 12:14:46 +0100

    Add files via upload

--- api/getZoneRelayState.php
+++ api/getZoneRelayState.php
@@ -23,9 +23,9 @@ getZoneStatus.php
 When called returns the current status of the Zone passed as parameter.
 
 E.g.
-http://192.168.1.2/api/getZoneStatus?zonename=Living%20Room
+http://192.168.1.2/api/getZoneRelayState?zonename=Downstairs
 
-{"success":true,"status":"0","temp":"18.3","datetime":"2021-02-28 20:53:39","bat_voltage":"2.50","bat_level":"43.00"}
+{{"success":true,"relays":[{"zone_name":"Downstairs","relay_name":"Downstairs","state":"0"}]}
 */
 
 header("Access-Control-Allow-Origin: *");
@@ -39,7 +39,7 @@ require_once(__DIR__.'../../st_inc/functions.php');
 
 if(isset($_GET['zonename'])) {
         $zonename = $_GET['zonename'];
-        $query = "SELECT `r`.`name`, `r`.`state`
+        $query = "SELECT `r`.`name` AS `relay_name`, IF(`r`.`state` = 0,'OFF','ON') AS `state`, `z`.`name` AS `zone_name`
 		FROM `zone_relays`
 		JOIN `zone` `z` ON `z`.`id` = `zone_relays`.`zone_id`
 		JOIN `relays` `r` ON `r`.`id` = `zone_relays`.`zone_relay_id`
@@ -51,17 +51,37 @@ if(isset($_GET['zonename'])) {
                 echo json_encode(array("success" => False, "state" => "No Zone with that name found OR No Relays Associate."));
         } else {
 		$relays = array();
-		do {
+		while($row = mysqli_fetch_assoc($results)) {
 			$relays[] = array(
-                        "name"        => $row['name'],
+                        "zone_name"        => $row['zone_name'],
+                        "relay_name"        => $row['relay_name'],
                         "state" => $row['state']
                 	);
-                } while($row = mysqli_fetch_assoc($results));
+                };
                 http_response_code(200);
                 echo json_encode(array("success" => True, "relays" => $relays));
 	}
 } else {
-        http_response_code(400);
-        echo json_encode(array("success" => False, "state" => "Data is incomplete."));
+        $query = "SELECT `r`.`name` AS `relay_name`, IF(`r`.`state` = 0,'OFF','ON') AS `state`, `z`.`name` AS `zone_name`
+                FROM `zone_relays`
+                JOIN `zone` `z` ON `z`.`id` = `zone_relays`.`zone_id`
+                JOIN `relays` `r` ON `r`.`id` = `zone_relays`.`zone_relay_id`;";
+        $results = $conn->query($query);
+        $rowcount=mysqli_num_rows($results);
+        if ($rowcount == 0) {
+                http_response_code(400);
+                echo json_encode(array("success" => False, "state" => "No Zones OR No Relays Found."));
+        } else {
+                $relays = array();
+                while($row = mysqli_fetch_assoc($results)) {
+                        $relays[] = array(
+                        "zone_name"        => $row['zone_name'],
+                        "relay_name"        => $row['relay_name'],
+                        "state" => $row['state']
+                        );
+                };
+                http_response_code(200);
+                echo json_encode(array("success" => True, "relays" => $relays));
+        }
 }
 ?>
