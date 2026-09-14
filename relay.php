8bc4be30db3a9ebf302e4ee541185724ae3d36c4
twa127 <46624596+twa127@users.noreply.github.com>
2026-09-14 08:11:27 +0100

    Update relay.php

--- relay.php
+++ relay.php
@@ -73,7 +73,7 @@ if (isset($_POST['submit'])) {
 	$group_id = $_POST['group_id'];
 	//Add or Edit relay record to relays Table
         $query = "INSERT INTO `relays` (`id`, `sync`, `purge`, `relay_id`, `relay_child_id`, `name`, `type`, `index_id`, `pre_post`, `show_it`, `message_in`,`on_trigger`, `lag_time`,
-                `group_id`, `schedule_prev`,`schedule`, `sch_time_id`,`user_display`, `previous_state`, `state`, `current_val_2`, `fail_timeout`)
+                `group_id`, `schedule_prev`,`schedule`, `sch_time_id`,`user_display`, `restore_state`, `state`, `current_val_2`, `fail_timeout`)
                 VALUES ('{$id}', '{$sync}', '{$purge}', '{$selected_relay_id}', '{$relay_child_id}', '{$name}', '{$type}', '{$index_id}', 0, 1, 0, '{$on_trigger}',
                 '{$lag_time}',  '{$group_id}', 0, 0, 0, 0, 0, 0, 0, '{$fail_timeout}')
                 ON DUPLICATE KEY UPDATE sync=VALUES(sync), `purge`=VALUES(`purge`), relay_id='{$selected_relay_id}', relay_child_id='{$relay_child_id}', name=VALUES(name),
